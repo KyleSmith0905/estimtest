9b56397401b8c81111d487cdc83002a38434280d
Kyle Smith <kylesmith090502@gmail.com>
2023-03-01 15:45:55 -0500

    feature: Added Angular output target.

--- packages/estimtest-core/stencil.config.ts
+++ packages/estimtest-core/stencil.config.ts
@@ -1,42 +1,48 @@
 import { Config } from '@stencil/core';
 import { reactOutputTarget } from '@stencil/react-output-target';
+import { angularOutputTarget } from '@stencil/angular-output-target';
 import { sass } from '@stencil/sass';
 
 export const config: Config = {
-  namespace: 'estimtest-core',
-  taskQueue: 'async',
-  plugins: [
-    sass({
-      injectGlobalPaths: ['./src/styles/index.scss'],
-    })
-  ],
-  outputTargets: [
-    reactOutputTarget({
-      componentCorePackage: 'estimtest-core',
-      proxiesFile: '../estimtest-react/src/components.ts',
-    }),
-    {
-      type: 'dist-custom-elements',
-      dir: 'components'
-    },
-    {
-      type: 'dist',
-      esmLoaderPath: '../loader',
-    },
-    {
-      type: 'docs-readme',
-    },
-    {
-      type: 'www',
-      serviceWorker: null,
-    },
-  ],
-  buildEs5: 'prod',
-  extras: {
-    initializeNextTick: true,
-    scriptDataOpts: true
-  },
-  devServer: {
-    reloadStrategy: 'pageReload',
-  },
+	namespace: 'estimtest-core',
+	taskQueue: 'async',
+	plugins: [
+		sass({
+			injectGlobalPaths: ['./src/styles/index.scss'],
+		})
+	],
+	outputTargets: [
+		reactOutputTarget({
+			componentCorePackage: 'estimtest-core',
+			proxiesFile: '../estimtest-react/src/components.ts',
+		}),
+		angularOutputTarget({
+			componentCorePackage: 'estimtest-core',
+			directivesProxyFile: '../estimtest-angular/src/directives/components.ts',
+			directivesArrayFile: '../estimtest-angular/src/directives/index.ts',
+		}),
+		{
+			type: 'dist-custom-elements',
+			dir: 'components'
+		},
+		{
+			type: 'dist',
+			esmLoaderPath: '../loader',
+		},
+		{
+			type: 'docs-readme',
+		},
+		{
+			type: 'www',
+			serviceWorker: null,
+		},
+	],
+	buildEs5: 'prod',
+	extras: {
+		initializeNextTick: true,
+		scriptDataOpts: true
+	},
+	devServer: {
+		reloadStrategy: 'pageReload',
+	},
 };
