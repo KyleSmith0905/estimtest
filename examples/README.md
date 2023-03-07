8f6671a6982a59b001daf5f7b3d4a7c252decb87
Kyle Smith <kylesmith090502@gmail.com>
2023-03-07 12:30:51 -0500

    feature: Implemented conditional loading to examples.

--- examples/README.md
+++ examples/README.md
@@ -2,11 +2,11 @@
 
 All examples should have minimal editing applied from the default "getting started" app; the only thing changed should be related to adding Estimtest to the app. Unless specified the all scrips will not have Estimtest active.
 
-| Tools | Build Size Increase[^1] |
+| Tools | Before Estimtest[^1] | After Estimtest[^1] |
 |---|---|
 | Angular | n/a |
 | Next JS | n/a |
-| Vite React | n/a |
-| Vite Vue | 112 Bytes |
+| Vite React | 53040B | 53545B |
+| Vite Vue | 112 Bytes | 109316B |
 
-[^1]: Build size increase is measured by counting the number of characters that seemed to be added. We are considering that the implementation of the library is minimal (therefore: No classes, no properties, and short variable names).
+[^1]: Size of application is measured by using Chrome's dev tools and see the amount of bytes transferred. Both builds were production. `Before Estimtest` sizes were measured by entirely removing estimtest from the project.
--- examples/vite-react/src/main.tsx
+++ examples/vite-react/src/main.tsx
@@ -1,18 +1,30 @@
-import React from 'react'
+import React, { lazy, Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import { EstimtestCore, defineCustomElements } from 'estimtest-react';
 
-defineCustomElements();
+const LazyEstimtest = lazy(async () => {
+  if (import.meta.env.VITE_ESTIMTEST === 'true') {
+    const { EstimtestCore, defineCustomElements } = await import('estimtest-react')
+    defineCustomElements();
+
+    return ({default: EstimtestCore});
+  }
+  else {
+    return ({default: (() => <div/>) as any});
+  }
+});
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
-    <EstimtestCore
-      active={true}
-      // full-screen is a css class defined in `index.css`
-      className='full-screen'
-    />
+    <Suspense>
+      <LazyEstimtest
+        active={true}
+        // full-screen is a css class defined in `index.css`
+        className='full-screen'
+      />
+    </Suspense>
   </React.StrictMode>,
 )
