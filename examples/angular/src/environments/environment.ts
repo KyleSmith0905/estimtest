1d56a16149489845448396ffcb53e5d0150d38c1
Kyle Smith <kylesmith090502@gmail.com>
2023-03-08 13:44:14 -0500

    meta (examples): Changed a comment to reference my fake components with Angular.

--- examples/angular/src/environments/environment.ts
+++ examples/angular/src/environments/environment.ts
@@ -4,7 +4,8 @@ import { EstimtestModule } from "estimtest-angular";
 
 /**
  * All imports are present, for type checking to not error when using a module that are not
- * imported on some environments. Ensure the module isn't needed if so.
+ * imported on some environments. We will be warned whether the module is in use or not, therefore
+ * you could add kinda fake component.
  */
 const environment = {
   estimtest: false,
